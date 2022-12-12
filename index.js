1b3f2dab53fc7d1b641f61ab70e4fc2c1e1c8c60
Cryogenic <cryogenic.vlog@gmail.com>
2022-12-11 18:14:52 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+let task = document.getElementById('task').value;
+let submit = document.getElementById('submit');
+let list = document.getElementById('list');
+
+submit.addEventListener("click",addTask);
+
+function addTask(){
+    let task = document.getElementById('task').value;
+  
+    let entry = document.createElement('li');
+    let cancel = document.createElement('button');
+    let x = document.createTextNode('X');
+    cancel.appendChild(x);
+    entry.appendChild(document.createTextNode(task));
+    entry.appendChild(cancel)
+    list.appendChild(entry)
+    cancel.addEventListener('click',() => {removeTask(entry)});
+   
+}
+
+function removeTask(t) {
+    list.removeChild(t)
+}
